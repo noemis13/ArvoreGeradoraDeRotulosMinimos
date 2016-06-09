b1a3ce48fd3b4b6cc7b7638967ef13babed5929a
noemis13 <noemischerer13@gmail.com>
2016-06-09 18:13:45 -0300

    parte do mvca

--- RotulosMinimos.py
+++ RotulosMinimos.py
@@ -13,7 +13,48 @@ def ler_arquivo (nomeArquivo):
 	
 	criaGrafo(cebecalho, conteudoDoArquivo)
 	arquivo.close()
+
+def criaGrafo(cabecalho, conteudoDoArquivo):
+	rotulo = cabecalho[1]
+	tamLaco = int(cabecalho[0])
 	
+	vetorVertices = []
+	valorVertice = 1
+	
+	valor = 0
+	contLinhas = 0
+	
+	for i in conteudoDoArquivo:
+		contColunas = 0
+		for j in i:
+			if(tamLaco == 0):
+				grafo = {}
+				vertice = {}
+				valorVertice = 1
+				
+			contColunas = contColunas + 1
+			vertice = {'posLinha': contLinhas, 'posColuna': contColunas, 'valorDoVertice': valorVertice}
+			
+			if(j != rotulo):
+				valor = encontraVertice(vertice, contLinhas, contColunas)
+				vetorVertice = contLinhas, contColunas
+				grafo ={'vertice': valor, 'ligado': vetorVertice, 'rotulo': j}	
+			
+			valorVertice = valorVertice + 1
+			print grafo
+			mvca(grafo)
+		contLinhas = contLinhas + 1
+		tamLaco = tamLaco - 1
+		
+		
+def encontraVertice (vertice, contLinhas, contColunas):				
+	for i in vertice: 
+		if(contLinhas == vertice.get('posLinha') and contColunas == vertice.get('posColuna')):
+			valor = vertice.get('valorDoVertice')
+	return valor
+
+
+"""	
 def criaGrafo(cabecalho, conteudoDoArquivo):
 	rotulo = cabecalho[1]
 	tamLaco = int(cabecalho[0])
@@ -51,7 +92,7 @@ def encontraVertice (vertice, contLinhas, contColunas):
 		if(contLinhas == vertice.get('posLinha') and contColunas == vertice.get('posColuna')):
 			valor = vertice.get('valorDoVertice')
 	return valor			
-		
+"""		
 """
 -------------PASSOS DO MVCA-----------
 1) pegar label por label
